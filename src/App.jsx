1475b9070068d9ad42396d6461c1add8f273f37b
Deividds1 <deivid1421@gmail.com>
2023-07-02 13:22:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ onRestart }) {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = [...squares];
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function handleRestart() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+    onRestart();
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else if (!squares.includes(null)) {
+    status = 'It\'s a draw!';
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <div className="board">
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
+      </div>
+      <button className="restart-button" onClick={handleRestart}>Restart Game</button>
+    </div>
+  );
+}
+
+function Game() {
+  const [gameKey, setGameKey] = useState(0);
+
+  function handleRestart() {
+    setGameKey(gameKey + 1);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board key={gameKey} onRestart={handleRestart} />
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game;
